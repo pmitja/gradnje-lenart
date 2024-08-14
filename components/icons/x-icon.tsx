841c361dddfd6786d07fae739f26e7a0ed661ca9
MinejPak <minejpak@yahoo.com>
2024-08-14 23:10:24 +0200

    Add x-icon

---
+++ components/icons/x-icon.tsx
@@ -0,0 +1,22 @@
+import type { FC, SVGProps } from 'react';
+
+const XIcon: FC<SVGProps<SVGSVGElement>> = (props) => (
+  <svg 
+    width="52" 
+    height="52" 
+    viewBox="0 0 52 52" 
+    fill="none" 
+    stroke="currentColor" 
+    xmlns="http://www.w3.org/2000/svg" 
+    {...props}>
+    <path 
+      d="M1 51L51 1M1 1L51 51" 
+      stroke="#9A1F18" 
+      strokeWidth="2" 
+      strokeLinecap="round" 
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default XIcon;

0fa4b771d3f2a9ebd9f407408b9ec3d198454b7f
MinejPak <minejpak@yahoo.com>
2024-08-14 23:11:57 +0200

    Add onClick prop to ButtonWithIcon component

--- components/common/button-with-icon/index.tsx
+++ components/common/button-with-icon/index.tsx
@@ -2,8 +2,8 @@ import ArrowRightIcon from "@/components/icons/arrow-right";
 import { Button, ButtonProps } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
-const ButtonWithIcon = ({ variant, children, size, className }: ButtonProps) => {
-  return ( <Button variant={variant} size={size} className={cn("flex gap-[6px] h-fit", className)}>{children}<ArrowRightIcon /></Button> );
+const ButtonWithIcon = ({ variant, children, size, className, onClick }: ButtonProps) => {
+  return ( <Button variant={variant} size={size} onClick={onClick} className={cn("flex gap-[6px] h-fit", className)}>{children}<ArrowRightIcon /></Button> );
 }
  
 export default ButtonWithIcon;

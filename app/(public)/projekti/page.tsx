1e0984face9018a6f57d33b2f927695599ad963f
MinejPak <minejpak@yahoo.com>
2024-08-16 22:54:40 +0200

    Utilize the OffersBanner component

--- app/(public)/projekti/page.tsx
+++ app/(public)/projekti/page.tsx
@@ -2,6 +2,9 @@ import ProjectFilterHero from '@/components/common/hero/project-filter-hero'
 import ProccesOfBuying from '@/components/common/procces-of-buying'
 import Faq from '@/components/layouts/sections/faq'
 import ProjectsSection from '@/components/layouts/sections/projects-section'
+import OffersBanner from '@/components/layouts/sections/offers-banner'
+
+const OffersBannerImages = ['/stanovanje.jpg', '/stanovanje2.jpg', '/stanovanje3.jpg']
 
 const ActualProjectPage = () => {
   return (
@@ -9,6 +12,12 @@ const ActualProjectPage = () => {
       <ProjectFilterHero />
       <ProjectsSection />
       <ProccesOfBuying />
+      <OffersBanner
+        title='Ne spreglejte te ponudbe'
+        buttonLabel='Več o nepremičnini'
+        description='Pri nas vam omogočamo najti domove, ki ustrezajo vašemu življenjskemu slogu in aspiracijam. Odkrijte brezhibno nepremičninsko izkušnjo.'
+        images={OffersBannerImages}
+      />
       <Faq />
     </>
   )

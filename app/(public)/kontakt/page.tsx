5d6515604899f6c30c4a8a256576f485c531d9af
pmitja <pmitja1@gmail.com>
2024-10-16 21:46:48 +0200

    Add contact page.

---
+++ app/(public)/kontakt/page.tsx
@@ -0,0 +1,88 @@
+import { Metadata } from 'next'
+import Image from 'next/image'
+import React from 'react'
+
+import ContactUs from '@/components/common/contact-us'
+import Cta from '@/components/common/cta'
+import GradnjeIcon from '@/components/icons/gradnje-icon'
+import OffersBanner from '@/components/layouts/sections/offers-banner'
+import ShadowBanner from '@/components/ui/shadow-banner'
+
+export const metadata: Metadata = {
+  title: 'Kontakt | Vaše Podjetje',
+  description: 'Stopite v stik z nami za vse vaše nepremičninske potrebe.',
+}
+
+const KontaktPage = () => (
+  <div>
+    <ShadowBanner
+      backgroundImagePath="/about-hero-image.webp"
+      heading="Gradnje Plus"
+      icon={<GradnjeIcon className="h-20 w-24 fill-primary-200" />}
+    />
+    <section className="relative inset-x-1/2 mx-[-50vw] w-screen min-w-[100vw] bg-primary-75">
+      <div className="container flex w-full justify-between gap-4 overflow-hidden py-8 lg:py-[70px]">
+        <Image
+          src={'/gradnje-plus-logo.webp'}
+          alt="Partner 1"
+          width={150}
+          height={100}
+          className="size-auto object-contain"
+        />
+        <Image
+          src={'/gradnje-plus-logo.webp'}
+          alt="Partner 2"
+          width={150}
+          height={100}
+          className="size-auto object-contain"
+        />
+        <Image
+          src={'/gradnje-plus-logo.webp'}
+          alt="Partner 3"
+          width={150}
+          height={100}
+          className="hidden size-auto object-contain md:block"
+        />
+        <Image
+          src={'/gradnje-plus-logo.webp'}
+          alt="Partner 4"
+          width={150}
+          height={100}
+          className="hidden size-auto object-contain md:block"
+        />
+        <Image
+          src={'/gradnje-plus-logo.webp'}
+          alt="Partner 5"
+          width={150}
+          height={100}
+          className="hidden size-auto object-contain lg:block"
+        />
+        <Image
+          src={'/gradnje-plus-logo.webp'}
+          alt="Partner 6"
+          width={150}
+          height={100}
+          className="hidden size-auto object-contain lg:block"
+        />
+        <Image
+          src={'/partners-pattern.webp'}
+          alt="Partner pattern"
+          width={170}
+          height={150}
+          className="absolute bottom-0 right-0 hidden md:block md:max-h-[100px] md:max-w-[120px] lg:max-h-[180px] lg:max-w-[180px]"
+        />
+      </div>
+    </section>
+    <section className="py-6 lg:py-12">
+      <ContactUs />
+    </section>
+    <section className="py-6">
+      <Cta />
+    </section>
+    <section className="py-6">
+      <OffersBanner />
+    </section>
+  </div>
+)
+
+export default KontaktPage
--- components/ui/shadow-banner.tsx
+++ components/ui/shadow-banner.tsx
@@ -4,7 +4,7 @@ type ShadowBannerProps = {
   backgroundImagePath: string
   icon: React.ReactNode
   heading: string
-  subheading: string
+  subheading?: string
 }
 
 const ShadowBanner = ({ backgroundImagePath, icon, heading, subheading }: ShadowBannerProps) => (
@@ -15,14 +15,14 @@ const ShadowBanner = ({ backgroundImagePath, icon, heading, subheading }: Shadow
     }}
   >
     <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black/60"></div>
-    <div className="container relative flex size-full flex-col items-start justify-end gap-8 overflow-hidden pb-8 lg:w-full lg:grid-cols-2 lg:gap-5 lg:pb-28 lg:pt-20">
+    <div className="container relative flex size-full flex-col items-start justify-end gap-8 overflow-hidden pb-8 lg:w-full lg:grid-cols-2 lg:gap-5 lg:pb-8 lg:pt-20">
       <div className="flex items-center justify-start gap-3 text-body-200">
         {icon}
         <h1 className="text-3xl font-bold uppercase md:text-4xl">{heading}</h1>
       </div>
-      <p className="max-w-5xl text-3xl font-bold leading-snug text-body-200 md:text-4xl">
+      {subheading && <p className="max-w-5xl text-3xl font-bold leading-snug text-body-200 md:text-4xl">
         {subheading}
-      </p>
+      </p>}
     </div>
   </div>
 )

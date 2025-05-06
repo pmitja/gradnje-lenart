f5d958f7c99e78e628172e3f3fac94aad5d252d8
pmitja <pmitja1@gmail.com>
2025-05-06 08:52:27 +0200

    Replace FeaturedProperties component with PropertyListingSection in the Home page layout for improved property display.

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,14 +1,14 @@
-import FeaturedProperties from '@/components/common/featured-properties'
 import Hero from '@/components/common/hero'
 import PartnersBanner from '@/components/common/partners-banner'
 import Testimonials from '@/components/common/testimonials'
 import WhyChooseUs from '@/components/common/why-choose-us'
+import PropertyListingSection from '@/components/layouts/sections/property-listing-section'
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <Hero />
-      <FeaturedProperties />
+      <PropertyListingSection />
       <PartnersBanner />
       <WhyChooseUs />
       <Testimonials />
